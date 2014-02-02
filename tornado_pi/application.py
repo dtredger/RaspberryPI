aec608f7d063b5a8c4942d470cb649648b26f795
dtredger <d.tredger@gmail.com>
2014-02-02 15:31:43 -0500

    all raspberry pi files in one repo

---
+++ tornado_pi/application.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+## -*- coding: utf-8 -*-
+import os
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        page_arry = ["TIV 1", "TIV 2"]
+        #self.set_cookie("cookie","cookieval")
+        self.render("home.html",title="catfishes",storm_chasers=page_arry)
+
+handlers = [
+    (r"/", MainHandler),
+]
+
+settings = dict(
+    template_path=os.path.join(os.path.dirname(__file__), "templates"),
+    static_path=os.path.join(os.path.dirname(__file__), "static"),
+
+)
+application = tornado.web.Application(
+    handlers, **settings)
+
+
+if __name__ == '__main__':
+    application.listen(os.environ.get('SERVER_PORT', 8080))
+    tornado.ioloop.IOLoop.instance().start()
