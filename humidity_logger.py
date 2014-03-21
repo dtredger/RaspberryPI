7cbceaf0a3fea66e30bcef289d55029432bac81b
dtredger <d.tredger@gmail.com>
2014-03-20 21:26:16 -0400

    diagram of sensor wiring

--- humidity_logger.py
+++ humidity_logger.py
@@ -1,36 +1,45 @@
 import os
 import time
 import datetime
-import serial
 
-try:
-    ser = serial.Serial(os.environ.get('ARDUINO'), 9600)
-except:
-    print "Find where yer Arduino is connected, and set that to an ARDUINO environment variable"
-
+DHT_BINARY = "./DHT_binary"
 
 def read_humidity():
-    try:
-        sensor = ser.readline()
-    except:
-        sensor = "could not read line.\n"
-    return sensor
+	try:
+		live_data = subprocess.check_output([DHT_BINARY, "2302", "4"])
+
+		temp_group = re.search("Temp =\s+([0-9.]+)", live_data)
+		if not temp_group:
+			time.sleep(3)
+			continue
+		temp = float(temp_group.group(1))
+		
+		humidity_group = re.search("Hum =\s+([0-9.]+)", live_data)
+		if not humidity_group:
+			time.sleep(3)
+			continue
+		humidity = float(humidity_group.group(1))
+
+		sensor_data = "T %s, H %s" % (temp, humidity)
+	except:
+		sensor_data = "could not read line.\n"
+	return sensor_data
 
 def log_output():
-    timestamp = datetime.datetime.utcnow().replace( second=0, microsecond=0).strftime("%Y-%m-%d_%H.%M")
-    filename = os.environ.get('HUMID_LOG','hl_') + str(timestamp)
-    try:
-        with open(filename, 'w') as file:
-            print "writing to %s" % filename
-            while time.time() < ( time.time() + 86400):    # one file per day
-                file.write(str(int(time.time())) + " - " + read_humidity())
-                file.flush()
-                print read_humidity()
-            file.close()
-    except:
-        print "couldn't write to %s" % filename
-        print "exiting."
-        raise SystemExit
+	timestamp = datetime.datetime.utcnow().replace(second=0, microsecond=0).strftime("%Y-%m-%d_%H.%M")
+	filename = os.environ.get('HUMIDITY_LOG','hl_') + str(timestamp)
+	try:
+		with open(filename, 'w') as file:
+			print "writing to %s" % filename
+			while time.time() < ( time.time() + 86400):	  # one file per day
+				file.write(str(int(time.time())) + ", " + read_humidity())
+				file.flush()
+				print read_humidity()
+			file.close()
+	except:
+		print "couldn't write to %s" % filename
+		print "exiting."
+		raise SystemExit
 
 while True:
-    log_output()
+	log_output()
