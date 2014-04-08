7b36885f819ff2fdbbc764831182ffce00a4347a
dtredger <d.tredger@gmail.com>
2014-04-07 20:03:51 -0400

    timelapse script captures images given interval

---
+++ camera_pi/timelapse_control.py
@@ -0,0 +1,28 @@
+import os
+import time
+
+TIMELAPSE_DIRECTORY = "/500gb_hd/timelapse/"
+
+def capture_timelapse_images(interval_minutes=60):
+    timestamp = int(time.time())
+        
+    # Define the size of the image you wish to capture. 
+    img_width = "800" # Max = 2592 
+    img_height = "600" # Max = 1944
+    
+    # Capture the image using raspistill. Set to capture with added sharpening, 
+    # auto white balance and average metering mode
+    os.system("raspistill -w " + img_width + " -h " + img_height + " -o " + \
+        TIMELAPSE_DIRECTORY + str(timestamp) + ".jpg -sh 40 -awb auto -mm average -v")
+    
+    # 60 seconds * 60 = 1/hr
+    time.sleep(60*int(interval_minutes))
+
+
+if __name__ == "__main__":
+    import sys
+    try:
+        while True:
+            capture_timelapse_images(sys.argv[1])
+    except KeyboardInterrupt:
+        pass
