c7ae932b036fa5dc9ac4f29cd144d8c8813c3734
Dylan Tredger <d.tredger@gmail.com>
2017-04-08 16:35:29 -0700

    local unveil

--- flask_image.py
+++ flask_image.py
@@ -32,7 +32,54 @@ img {
 }
     </style>
     <script src="https://code.jquery.com/jquery-1.10.2.min.js" charset="utf-8"></script>
-    <script src="http://luis-almeida.github.io/unveil/jquery.unveil.min.js" charset="utf-8"></script>
+    <script>
+        ;(function($) {
+
+          $.fn.unveil = function(threshold, callback) {
+
+            var $w = $(window),
+                th = threshold || 0,
+                retina = window.devicePixelRatio > 1,
+                attrib = retina? "data-src-retina" : "data-src",
+                images = this,
+                loaded;
+
+            this.one("unveil", function() {
+              var source = this.getAttribute(attrib);
+              source = source || this.getAttribute("data-src");
+              if (source) {
+                this.setAttribute("src", source);
+                if (typeof callback === "function") callback.call(this);
+              }
+            });
+
+            function unveil() {
+              var inview = images.filter(function() {
+                var $e = $(this);
+                if ($e.is(":hidden")) return;
+
+                var wt = $w.scrollTop(),
+                    wb = wt + $w.height(),
+                    et = $e.offset().top,
+                    eb = et + $e.height();
+
+                return eb >= wt - th && et <= wb + th;
+              });
+
+              loaded = inview.trigger("unveil");
+              images = images.not(loaded);
+            }
+
+            $w.on("scroll.unveil resize.unveil lookup.unveil", unveil);
+
+            unveil();
+
+            return this;
+
+          };
+
+        })(window.jQuery || window.Zepto);
+    </script>
     <script>
 $(document).ready(function() {
     $('img').unveil(1000);
